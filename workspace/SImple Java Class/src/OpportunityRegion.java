fc7a5bd80abed076126ea9386c505ea2f0b73d8b
sridhar <sridhar@SsssY.Home>
2016-11-13 23:02:45 +0000

    First commit from Home PC

---
+++ workspace/SImple Java Class/src/OpportunityRegion.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+
+/**
+ * @author sridhar
+ *
+ */
+public class OpportunityRegion {
+
+	/**
+	 * @param args
+	 */
+	
+	public static String pb2;
+	public String GB;
+
+	
+static String pb(String Reg){
+		
+		if (Reg == "UK"){
+					
+		pb2="abcdef";
+		}
+		
+		return pb2;
+
+	}
+
+	public static void main(String[] args) {
+		
+		final String prcbk= new String(args[0]);
+
+		OpportunityRegion.pb(prcbk);	
+		
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+}
